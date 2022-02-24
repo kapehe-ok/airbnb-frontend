69c68a1b5a114720def6aebc693770b7e1040e4e
lowisren <lowisren@gmail.com>
2022-02-24 17:17:37 -0500

    create single property structure

--- pages/property/[slug].js
+++ pages/property/[slug].js
@@ -1,16 +1,51 @@
-import { sanityClient } from "../../sanity"
+import { sanityClient } from "../../sanity";
+import { isMultiple } from "../../utils";
 
-
-export const Property = ({ title }) => {
-    return (
-        <h1>{title}</h1>
-    )
-}
+export const Property = ({
+  title,
+  location,
+  propertyType,
+  mainImage,
+  images,
+  pricePerNight,
+  beds,
+  bedrooms,
+  description,
+  host,
+  reviews,
+}) => {
+  return (
+    <div className="container">
+      <h1>
+        <b>{title}</b>
+      </h1>
+      <h2>
+        <b>
+          {propertyType} hosted by {host?.name}
+        </b>
+      </h2>
+      <h3>
+        {bedrooms} bedroom{isMultiple(bedrooms)} * {beds} bed{isMultiple(beds)}
+      </h3>
+      <hr />
+      <h4>Enhanced Clean</h4>
+      <p>
+        This host is committed to AirBnb's 5-step enhanced cleaning process.
+      </p>
+      <h4>Amenities for everyday living</h4>
+      <p>The host has equipped this property with everything!</p>
+      <h4>Enhanced Clean</h4>
+      <p>
+        This host is committed to AirBnb's 5-step enhanced cleaning process.
+      </p>
+    </div>
+  );
+};
 
 export const getServerSideProps = async (pageContext) => {
-    const pageSlug = pageContext.query.slug
+  const pageSlug = pageContext.query.slug;
 
-    const query = `*[_type == "property" && slug.current == $pageSlug][0]{
+  const query = `*[_type == "property" && slug.current == $pageSlug][0]{
         title,
         location,
         propertyType,
@@ -35,32 +70,32 @@ export const getServerSideProps = async (pageContext) => {
                 image
             }
         }
-    }`
+    }`;
 
-    const property = await sanityClient.fetch(query, { pageSlug })
+  const property = await sanityClient.fetch(query, { pageSlug });
 
-    if (!property) {
-        return {
-            props: null,
-            notFound: true,
-        }
-    } else {
-        return {
-            props: {
-                title: property.title,
-                location: property.location,
-                propertyType: property.propertyType,
-                mainImage: property.mainImage,
-                images: property.images,
-                pricePerNight: property.pricePerNight,
-                beds: property.beds,
-                bedrooms: property.bedrooms,
-                description: property.description,
-                host: property.host,
-                reviews: property.reviews
-            }
-        }
-    }
-}
+  if (!property) {
+    return {
+      props: null,
+      notFound: true,
+    };
+  } else {
+    return {
+      props: {
+        title: property.title,
+        location: property.location,
+        propertyType: property.propertyType,
+        mainImage: property.mainImage,
+        images: property.images,
+        pricePerNight: property.pricePerNight,
+        beds: property.beds,
+        bedrooms: property.bedrooms,
+        description: property.description,
+        host: property.host,
+        reviews: property.reviews,
+      },
+    };
+  }
+};
 
-export default Property
+export default Property;
